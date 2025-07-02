fc853506ee02553b3c24d8db25015f41d56fa3c8
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-07-03 00:03:32 +0600

    Solved second problem

---
+++ secondPracticeProblem.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int integerValue;
+    Node *nextPointer;
+
+    Node(int integerValue)
+    {
+        this->integerValue = integerValue;
+        this->nextPointer = NULL;
+    }
+};
+
+void insertAtTail(Node *&head, Node *&tail, int integerValue)
+{
+    Node *newNode = new Node(integerValue);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        tail->nextPointer = newNode;
+        tail = newNode;
+    }
+}
+
+void printReverse(Node *head)
+{
+    Node *tmp = head;
+    if (tmp == NULL)
+    {
+        return;
+    }
+
+    printReverse(tmp->nextPointer);
+    cout << tmp->integerValue << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int inputValue;
+    while (true)
+    {
+        cin >> inputValue;
+        if (inputValue == -1)
+        {
+            break;
+        }
+        else
+        {
+            insertAtTail(head, tail, inputValue);
+        }
+    }
+
+    printReverse(head);
+
+    return 0;
+}
